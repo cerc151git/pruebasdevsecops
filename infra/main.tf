42a75b1ab63b422f66aaca1f0151e8657d653912
cerc151git <cerc151@gmail.com>
2026-05-14 17:29:12 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,30 @@
 provider "azurerm" {
   features {}
-
 }
+
 resource "azurerm_resource_group" "devsecopsapp" {
     name = var.rg_name
     location = var.location
+}
+
+resource "azurerm_service_plan" "serviceplan_web" {
+    name = "webprueba-plan"
+    resource_group_name = azurerm_resource_group.devsecopsapp.name
+    location = azurerm_resource_group.devsecopsapp.location
+    os_type = "Linux"
+    sku_name = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "paginaprueba" {
+    name = "paginaprueba"
+    resource_group_name = azurerm_resource_group.devsecopsapp.name
+    location = azurerm_resource_group.devsecopsapp.location
+    service_plan_id = azurerm_service_plan.serviceplan_web.id
+    site_config {
+      always_on = true
+      application_stack {
+        docker_image_name = var.imagencontenedor
+        docker_registry_url = "https://index.docker.io"
+      }
+    }
 }
